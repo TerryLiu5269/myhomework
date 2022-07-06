4887fe8f4f8ed251ed155c015d9442a1a16b92e6
terryliu <terryliu@fstop.com.tw>
2022-07-06 08:43:08 +0800

    css微調

--- src/app/mychart/mychart.component.ts
+++ src/app/mychart/mychart.component.ts
@@ -15,11 +15,12 @@ export class MychartComponent implements OnInit {
   // Doughnut
   public doughnutChartLabels: string[] = ['近7天收支', '近7天轉入金額', '近7天轉出金額', '近7天利息'];
   public doughnutChartData: ChartData<'doughnut'> = {
-    datasets: [
-      // { data: [350, 450, 100, 250] }
-      { data: this.doughnutChData }
-    ],
-    labels: this.doughnutChartLabels
+    // labels: this.doughnutChartLabels,
+    datasets:
+      [
+        // { data: [350, 450, 100, 250] }
+        { data: this.doughnutChData }
+      ]
   };
   public doughnutChartType: ChartType = 'doughnut';
 
